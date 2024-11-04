3a57649c32ecc6f612c6f85b254682e0e721fc4b
Matteo Galdi <28713053+matteogaldi@users.noreply.github.com>
2024-11-04 21:06:05 +0100

    Initial Commit

---
+++ HackerNewsReader/HackerNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsReaderApp.swift
+//  HackerNewsReader
+//
+//  Created by Matteo Galdi on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
